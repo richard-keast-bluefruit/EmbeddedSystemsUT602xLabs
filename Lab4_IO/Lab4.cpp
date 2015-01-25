9e32462c9bf9854fd51f718a492c1002bec49788
richard.keast <richard.keast@bluefruit.co.uk>
2015-01-25 10:40:20 +0000

    - Added real RGB Led and switch classes for Launchpad. - refactored main, launchpad app to run Lab4 on the hardware, all works!

--- Lab4_IO/Lab4.cpp
+++ Lab4_IO/Lab4.cpp
@@ -1,8 +1,7 @@
 #include "Lab4.h"
 
 Lab4::Lab4(SwitchInterface &switch1, SwitchInterface &switch2, RGBLEDInterface &rgbLED)
-   : 
-   m_switch1(switch1),
+ : m_switch1(switch1),
    m_switch2(switch2),
    m_rgbLED(rgbLED)
 {
---
+++ Source/LaunchPadSwitches.h
@@ -0,0 +1,13 @@
+#ifndef LAUNCHPAD_SWITCHES_H
+#define LAUNCHPAD_SWITCHES_H
+
+struct Switches
+{
+   enum Value
+   {
+      Switch1 = 0x10,
+      Switch2 = 0x01
+   };
+};
+
+#endif

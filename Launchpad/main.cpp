da3bcbdd63a32d13ae2d1247659ada9fa252a672
richard.keast <richard.keast@bluefruit.co.uk>
2015-05-22 11:34:02 +0100

    added eclipse workspace

---
+++ Launchpad/main.cpp
@@ -0,0 +1,18 @@
+//#include "LaunchPad.h"
+//#include <cstdio>
+
+int main()
+{  
+   //LaunchPad launchPad;
+   
+   while(1)
+	{
+      //launchPad.Run();
+	   for (int i = 0; i < 1000000; i++)
+	   {
+		   int j = 55;
+		   int k = 23;
+		   j += k;
+	   }
+	}
+}
